af862616cbc2877e7d9a5dd661b73c1591acad61
junjihe <junji.he@biomind.ai>
2020-03-18 14:43:48 +0800

    牧场 init

---
+++ page/component/index.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+    imgUrls: [
+      '/image/b1.jpeg',
+      '/image/b2.jpeg',
+      '/image/b3.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 3000,
+    duration: 800,
+  }
+})
